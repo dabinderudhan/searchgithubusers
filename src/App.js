e7625a733883f6abea1d40cf4728cc608d8c882f
dabinder.udhan@gmail.com <dabinder.udhan@gmail.com>
2022-12-14 08:16:39 +0530

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 
 function App() {
   return (
     <AuthWrapper>
       <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <HashRouter basename="/">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </HashRouter>
       </Router>
     </AuthWrapper>
   );
