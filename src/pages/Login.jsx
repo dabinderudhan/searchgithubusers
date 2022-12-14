e5e6dd9386ea86915d008ca5b00173889d04cfcc
dabinder.udhan@gmail.com <dabinder.udhan@gmail.com>
2022-12-14 18:39:52 +0530

    input added in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useGlobalContext } from "../context/context";
 import styled from "styled-components";
 import loginImg from "../images/login-img.svg";
 
 const Login = () => {
+  const [githubUsername, setGithubUsername] = useState("");
+  const { searchGithubUser } = useGlobalContext();
   const { loginWithRedirect } = useAuth0();
 
   // console.log(useAuth0());
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchGithubUser(githubUsername);
+  };
 
   return (
     <Wrapper>
       <div className="container">
         <img src={loginImg} alt="github-user" />
         <h1>github user</h1>
-        <button className="btn" onClick={loginWithRedirect}>
-          Log In / Sign Up
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="enter your github username"
+            value={githubUsername}
+            onChange={(e) => setGithubUsername(e.target.value)}
+            required
+          />
+          <button className="btn" onClick={loginWithRedirect}>
+            Log In / Sign Up
+          </button>
+        </form>
       </div>
     </Wrapper>
   );
@@ -34,6 +52,22 @@ const Wrapper = styled.section`
   }
   h1 {
     margin-bottom: 1.5rem;
+  }
+  form {
+    display: flex;
+    gap: 1rem;
+  }
+  input {
+    border-color: transparent;
+    outline-color: var(--clr-grey-10);
+    letter-spacing: var(--spacing);
+    color: var(--clr-grey-3);
+    padding: 0.25rem 0.5rem;
+  }
+  input::placeholder {
+    color: var(--clr-grey-3);
+    text-transform: capitalize;
+    letter-spacing: var(--spacing);
   }
 `;
 
